4c864bb7ca321a715ae9e6b12d0d82030cfd5d32
arijit83sarkar <mail2arijit83@gmail.com>
2022-08-08 01:12:46 +0530

    configuration is done

--- src/main/java/com/raven/webmvcapp/configuration/ApplicationConfiguration.java
+++ src/main/java/com/raven/webmvcapp/configuration/ApplicationConfiguration.java
@@ -1,8 +1,14 @@
 package com.raven.webmvcapp.configuration;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +16,11 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.raven.webmvcapp")
+@PropertySource("classpath:app-db.properties")
 public class ApplicationConfiguration {
+
+	@Autowired
+	private Environment environment;
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -19,5 +29,26 @@ public class ApplicationConfiguration {
 		resourceViewResolver.setSuffix(".jsp");
 
 		return resourceViewResolver;
+	}
+
+	@Bean(name = "jdbcDataSource")
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+
+		try {
+			dataSource.setDriverClassName(environment.getProperty("jdbc.driver"));
+			dataSource.setUrl(environment.getProperty("jdbc.url"));
+			dataSource.setUsername(environment.getProperty("jdbc.user"));
+			dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+			dataSource.setInitialSize(Integer.parseInt(environment.getProperty("dbcp2.initial-size")));
+			dataSource.setMaxIdle(Integer.parseInt(environment.getProperty("dbcp2.max-idle")));
+			dataSource.setDefaultQueryTimeout(Integer.parseInt(environment.getProperty("dbcp2.default-query-timeout")));
+			dataSource.setDefaultAutoCommit(Boolean.valueOf(environment.getProperty("dbcp2.default-auto-commit")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return dataSource;
 	}
 }
